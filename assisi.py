5d370e9c645cb74a9aeb05aab7fddffc165830b5
Eric T. Dawson <eric.t.dawson@gmail.com>
2017-09-27 14:55:08 -0400

    Add argument parsing, sig_dictionary handling, random signature generation, and some warnings

--- assisi.py
+++ assisi.py
@@ -1,5 +1,37 @@
 import sys
 import random
+import multiprocessing as mp
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", type = str,
+     help = "Input mutational proportions / counts file.", dest = "infile")
+    parser.add_argument("-d", dest="sigdict", type = str,
+    help = "A signature:proportion dictionary file, CSV format, one [sig,prop] pair per line")
+    parser.add_argument("-c", action="store_true",
+     help = "Input is raw counts, rather than proportions. Default: False", dest="iscounts", default = False)
+    parser.add_argument("-n", type = int, dest="num",
+     help = "The number of mutations to simulate. Default: 1000", default = 1000)
+    parser.add_argument("-m", type = int,
+     dest="numsamples", help = "The number of mutational spectra to simulate. Default: 1", default = 1)
+    parser.add_argument("-t", type = int, dest="threads", default = 1,
+     help = "The number of multiprocessing threads to use, parallelized by spectrum. Default: 1")
+
+    return parser.parse_args()
+
+def parse_sig_dict(dict_file):
+    d = {}
+    with open(dict_file, "r") as ifi:
+        for line in ifi:
+            tokens = line.strip().split(",")
+            if len(tokens) > 3:
+                print "ERROR: too many columsn in sig_dict file."
+                print "There should be only two values, separated by a comma, per line."
+                print "E.g. \"1,0.5\""
+                exit(9)
+            d[int(tokens[0].strip())] = float(tokens[1].strip())
+    return d
 
 
 def parse_sigs(sig_file):
@@ -52,14 +84,19 @@ def sample_sig(probs, number):
 
 ## Args:
 ##  sig_and_amt: a dictionary of the signature index
-##  and the amount of that signature as a percentage of total mutations ( a float.)
-## number: the total number of mutations to produce
-## probs: an array (list of lists), 30 x 96, containing the probabilities for each
-## mut type within each signature
-def sample_sig_list(sig_and_amt, number, probs, eps = 0.0):
+##      and the amount of that signature as a percentage of total mutations ( a float.)
+##  number: the total number of mutations to produce
+##  probs: an array (list of lists), 30 x 96, containing the probabilities for each
+##  mutation type within each signature
+def sample_sig_list(sig_and_amt, number, probs, num_sigs = 30, eps = 0.0):
+    if len(sig_and_amt) == 0 and eps < 0.01:
+        print "FAILING: low epsilon and no signature proportions specified"
+        print "Please specify either of these values to prevent running infinitely."
+        exit(9)
+
     cspp = sample_prob
-    sigs = [i for i in xrange(0, 30)]
-    amts = [0.0 for i in xrange(0, 30)]
+    sigs = [i for i in xrange(0, num_sigs)]
+    amts = [0.0 for i in xrange(0, num_sigs)]
     for i in sig_and_amt:
         amts[i - 1] = float(sig_and_amt[i])
     
@@ -80,24 +117,31 @@ def sample_sig_list(sig_and_amt, number, probs, eps = 0.0):
             count += 1
     return ret
 
-def print_dist(sig):
+def print_dist(sig, use_tight_ranges = False):
     ostr = ""
-    ranges = [0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6,
-     0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05]
-    #ranges = [0.99,0.98,0.97,0.96,0.95,0.94,
-    # 0.93,0.92,0.91,0.90,0.89,0.88,0.87,0.86,
-    # 0.85,0.84,0.83,0.82,0.81,0.80,0.79,0.78,
-    # 0.77,0.76,0.75,0.74,0.73,0.72,0.71,0.70,
-    # 0.69,0.68,0.67,0.66,0.65,0.64,0.63,0.62,
-    # 0.61,0.60,0.59,0.58,0.57,0.56,0.55,0.54,
-    # 0.53,0.52,0.51,0.50,0.49,0.48,0.47,0.46,
-    # 0.45,0.44,0.43,0.42,0.41,0.40,0.39,0.38,
-    # 0.37,0.36,0.35,0.34,0.33,0.32,0.31,0.30,
-    # 0.29,0.28,0.27,0.26,0.25,0.24,0.23,0.22,
-    # 0.21,0.20,0.19,0.18,0.17,0.16,0.15,0.14,
-    # 0.13,0.12,0.11,0.10,0.9,0.8,0.7,0.6,0.5,
-    # 0.4,0.3,0.2,0.1, 0.01]
+    ranges = []
+    if use_tight_ranges:
+        ranges = [0.99,0.98,0.97,0.96,0.95,0.94,
+        0.93,0.92,0.91,0.90,0.89,0.88,0.87,0.86,
+        0.85,0.84,0.83,0.82,0.81,0.80,0.79,0.78,
+        0.77,0.76,0.75,0.74,0.73,0.72,0.71,0.70,
+        0.69,0.68,0.67,0.66,0.65,0.64,0.63,0.62,
+        0.61,0.60,0.59,0.58,0.57,0.56,0.55,0.54,
+        0.53,0.52,0.51,0.50,0.49,0.48,0.47,0.46,
+        0.45,0.44,0.43,0.42,0.41,0.40,0.39,0.38,
+        0.37,0.36,0.35,0.34,0.33,0.32,0.31,0.30,
+        0.29,0.28,0.27,0.26,0.25,0.24,0.23,0.22,
+        0.21,0.20,0.19,0.18,0.17,0.16,0.15,0.14,
+        0.13,0.12,0.11,0.10,0.09,0.08,.07,0.6,0.05,
+        0.04,0.03,0.02,0.01, 0.005]
+    else:
+        ranges = [0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6,
+        0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05]
+    
+    m = max(sig)
     for i in ranges:
+        if i > m:
+            continue
         for s in sig:
             if s >= i:
                 ostr += "|"
@@ -109,15 +153,45 @@ def print_dist(sig):
         ostr += "-="
     print ostr
     return ostr
+
+
 if __name__ == "__main__":
     #test = [random.random() for i in xrange(0, 96)]
     #print_dist(test)
     #print test
+    args = parse_args()
 
-    probs = parse_sigs(sys.argv[1])
-    sigm = {}
+    probs = parse_sigs(args.infile)
+    if args.iscounts:
+        probs = counts_to_props(probs)
+
+    n_muts = args.num
+    n_samples = args.numsamples
+
+    if n_samples > 4 and args.threads > 1:
+        print "threading not implemented"
+        exit()
+        p = mp.pool(args.threads)
+
+
+    sigm = {
+    }
+    if args.sigdict is not None:
+        sigm = parse_sig_dict(args.sigdict)
+    else:
+        rem = 1.0
+        while rem > 0.01:
+            i = random.randint(0, 30)
+            if i not in sigm:
+                nex = random.uniform(0, rem)
+                sigm[random.randint(0,30)] = nex
+                rem = rem - nex
+
+
+        print "Randomly simulating with", len(sigm), "signatures"
+        print sigm
     
-    d = sample_sig_list(sigm, 1000, probs)
+    d = sample_sig_list(sigm, n_muts, probs, len(probs), 0.0)
     d = counts_to_props(d)
-    print_dist(d)
+    print_dist(d, True)
     print d
